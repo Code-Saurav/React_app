f5c5def60e1987e23b3c7d19e5f9dc5c864dd097
Saurav Sah <sauravsah@Sauravs-MacBook-Pro.local>
2022-11-07 23:17:18 -0600

    initial commit

---
+++ react_nov7/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1> Hello World</h1>
+      <h2> We are starting this project on the Nov 7, 2022 : Due date : Nove 8, 2022</h2>
+    </div>
+  );
+}
+
+export default App;
